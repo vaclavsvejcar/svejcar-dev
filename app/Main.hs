e784696e7564f1a43c720d92701966754c0de503
Vaclav Svejcar <vaclav.svejcar@gmail.com>
2019-09-20 22:21:21 +0200

    Added support for viewing draft blog posts

--- app/Main.hs
+++ app/Main.hs
@@ -1,84 +1,112 @@
 {-# LANGUAGE OverloadedStrings #-}
 import qualified Svejcar.Dev.Tags              as SDT
-import           Control.Monad                  ( filterM )
+import           Control.Monad                  ( filterM
+                                                , when
+                                                )
 import           Data.List                      ( intersperse
                                                 , isSuffixOf
                                                 )
 import           Data.List.Split                ( splitOn )
 import           Hakyll
 import           Hakyll.Web.Sass                ( sassCompiler )
+import           System.Environment             ( getArgs
+                                                , withArgs
+                                                )
 import           System.FilePath                ( splitExtension )
 
 main :: IO ()
-main = hakyll $ do
+main = do
+  args <- getArgs
+  let draftMode  = length args == 2 && args !! 1 == "draft"
+      hakyllConf = if draftMode
+        then defaultConfiguration { destinationDirectory = "_draft"
+                                  , storeDirectory       = "_draft_cache"
+                                  , tmpDirectory         = "_draft_cache/tmp"
+                                  }
+        else defaultConfiguration
+      postsPattern = if draftMode
+        then "content/posts/*.md" .||. "content/drafts/*.md"
+        else "content/posts/*.md"
+      args' = take 1 args
 
-  tags <- buildTags "content/posts/*" (fromCapture "tags/*/index.html")
+  when draftMode $ putStrLn "!!!!!!!!! RUNNING IN DRAFT MODE !!!!!!!!!"
+  withArgs args' $ hakyllWith hakyllConf $ do
 
-  tagsRules tags $ \tag pattern -> do
-    route idRoute
-    compile $ do
-      list <- postList
-        tags
-        (\t -> recentFirst t
-          >>= filterM (fmap (elem tag) . getTags . itemIdentifier)
-        )
-      let ctx = constField "tag" tag <> constField "posts" list <> siteCtx tags
-      makeItem ""
-        >>= loadAndApplyTemplate "templates/posts-by-tag.html" ctx
-        >>= loadAndApplyTemplate "templates/default.html"      ctx
-        >>= relativizeUrls
-        >>= deIndexUrls
+    tags <- buildTags postsPattern (fromCapture "tags/*/index.html")
 
-  match "content/index.html" $ do
-    route stripContent
-    compile $ do
-      body <- fmap itemBody templateBodyCompiler
-      loadAllSnapshots "content/posts/*" "teaser"
-        >>= (fmap (take 100) . recentFirst)
-        >>= applyTemplateList body (postCtx tags <> bodyField "posts")
-        >>= makeItem
-        >>= loadAndApplyTemplate "templates/default.html" (siteCtx tags)
-        >>= relativizeUrls
-        >>= deIndexUrls
+    tagsRules tags $ \tag pattern -> do
+      route idRoute
+      compile $ do
+        list <- (postList postsPattern)
+          tags
+          (\t -> recentFirst t
+            >>= filterM (fmap (elem tag) . getTags . itemIdentifier)
+          )
+        let ctx =
+              constField "tag" tag <> constField "posts" list <> siteCtx tags
+        makeItem ""
+          >>= loadAndApplyTemplate "templates/posts-by-tag.html" ctx
+          >>= loadAndApplyTemplate "templates/default.html"      ctx
+          >>= relativizeUrls
+          >>= deIndexUrls
 
-  match "content/posts/*" $ do
-    route
-      $               directorizeDate
-      `composeRoutes` stripContent
-      `composeRoutes` setExtension "html"
-    compile $ do
-      compiled <- pandocCompiler
-      full <- loadAndApplyTemplate "templates/post.html" (postCtx tags) compiled
-      teaser <- loadAndApplyTemplate "templates/post-teaser.html" (postCtx tags)
-        $ dropMore compiled
-      saveSnapshot "content" full
-      saveSnapshot "teaser"  teaser
-      loadAndApplyTemplate "templates/default.html" (siteCtx tags) full
-        >>= relativizeUrls
-        >>= deIndexUrls
+    match "content/index.html" $ do
+      route stripContent
+      compile $ do
+        body <- fmap itemBody templateBodyCompiler
+        loadAllSnapshots postsPattern "teaser"
+          >>= (fmap (take 100) . recentFirst)
+          >>= applyTemplateList body (postCtx tags <> bodyField "posts")
+          >>= makeItem
+          >>= loadAndApplyTemplate "templates/default.html" (siteCtx tags)
+          >>= relativizeUrls
+          >>= deIndexUrls
 
-  match "images/*.jpg" $ do
-    route idRoute
-    compile copyFileCompiler
-  
-  match "css/*.css" $ do
-    route idRoute
-    compile copyFileCompiler
+    match postsPattern $ do
+      route
+        $               directorizeDate
+        `composeRoutes` stripContent
+        `composeRoutes` setExtension "html"
+      compile $ do
+        compiled <- pandocCompiler
+        full     <- loadAndApplyTemplate "templates/post.html"
+                                         (postCtx tags)
+                                         compiled
+        teaser <-
+          loadAndApplyTemplate "templates/post-teaser.html" (postCtx tags)
+            $ dropMore compiled
+        saveSnapshot "content" full
+        saveSnapshot "teaser"  teaser
+        loadAndApplyTemplate "templates/default.html" (siteCtx tags) full
+          >>= relativizeUrls
+          >>= deIndexUrls
 
-  match "css/*.scss" $ do
-    route $ setExtension "css"
-    let compressCssItem = fmap compressCss
-    compile (compressCssItem <$> sassCompiler)
+    match "images/*.jpg" $ do
+      route idRoute
+      compile copyFileCompiler
 
-  match "js/*.js" $ do
-    route idRoute
-    compile copyFileCompiler
+    match "css/*.css" $ do
+      route idRoute
+      compile copyFileCompiler
 
-  match "templates/*" $ compile templateBodyCompiler
+    match "css/*.scss" $ do
+      route $ setExtension "css"
+      let compressCssItem = fmap compressCss
+      compile (compressCssItem <$> sassCompiler)
 
-postList :: Tags -> ([Item String] -> Compiler [Item String]) -> Compiler String
-postList tags sortFilter = do
-  posts   <- sortFilter =<< loadAll "content/posts/*"
+    match "js/*.js" $ do
+      route idRoute
+      compile copyFileCompiler
+
+    match "templates/*" $ compile templateBodyCompiler
+
+postList
+  :: Pattern
+  -> Tags
+  -> ([Item String] -> Compiler [Item String])
+  -> Compiler String
+postList postsPattern tags sortFilter = do
+  posts   <- sortFilter =<< loadAll postsPattern
   itemTpl <- loadBody "templates/post-link.html"
   list    <- applyTemplateList itemTpl (postCtx tags) posts
   return list
